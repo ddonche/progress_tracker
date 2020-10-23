17ca13eaead228140f9b22511b37d12209cf5e7b
ddonche <dan@fatalfitness.com>
2020-10-23 14:44:08 -0600

    added new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Personal Tracker',
+      theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.red,
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+          )),
       home: MyHomePage(),
     );
   }
@@ -67,8 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Personal Tracker'),
-        backgroundColor: Colors.red[700],
+        title: Text(
+          'Personal Tracker',
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
@@ -94,7 +115,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[700],
         child: Icon(
           Icons.add,
         ),
